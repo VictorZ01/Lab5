a4500d893d6437ccbdec56fd879fc29833db32b3
VictorFries <victorzhen1vz@gmail.com>
2022-04-28 16:39:59 -0700

    first commit

---
+++ app/src/main/java/com/example/lab5/ToDoListActivity.java
@@ -0,0 +1,32 @@
+package com.example.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+public class ToDoListActivity extends AppCompatActivity {
+    public RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_list);
+
+        TodoListItemDao todoListItemDao = TodoDatabase.getSingleton(this).todoListItemDao();
+        List<TodoListItem> todoListItems= todoListItemDao.getAll();
+
+        TodoListAdapter adapter = new TodoListAdapter();
+        adapter.setHasStableIds(true);
+        adapter.setTodoListItems(todoListItems);
+
+        recyclerView=findViewById(R.id.todo_items);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+
+}
